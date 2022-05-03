b87e85a0bb2be305ff6331de4ffcbf24fb6a3b63
heskarioth <camilloshady@gmail.com>
2022-05-03 16:34:00 +0100

    adding invetory,prcingdataforauthor methods

--- data_parsers.py
+++ data_parsers.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-from typing import List
+from typing import List, Dict
 import re 
 
 
@@ -59,3 +59,49 @@ def parse_response_getPricingDataForAuthorTitleByBinding(results):
         if results['pricingInfoForBestAllConditions'] is None:
             raise ValueError('Book not found.')
         return pd.DataFrame(results['pricingInfoForBestAllConditions'],index=[0])
+
+
+def parse_response_getPricingDataForAuthorTitleBDP(response : List) -> Dict:
+    response = response[0]
+
+    dict_prices = {}
+    if response['success']==False:
+        return {'msg':'No book found with searching criterias.'}
+
+    if response['pricingInfoForBestUsed'] is None:
+        used_shipping_cost = np.nan
+        dict_prices['used_shipping_cost'] = np.nan
+        dict_prices['used_price'] = np.nan
+    else:
+        used_shipping_cost = response['pricingInfoForBestUsed']['domesticShippingPriceInPurchaseCurrencyWithCurrencySymbol'] if response['pricingInfoForBestUsed']['vendorCountryNameInSurferLanguage'] =='United Kingdom' else response['pricingInfoForBestUsed']['shippingToDestinationPriceInPurchaseCurrencyWithCurrencySymbol']
+        dict_prices['used_shipping_cost'] = float((re.compile(r'[^\d.,]+').sub('', used_shipping_cost)).replace(',','.'))
+        dict_prices['used_price'] = float(response['pricingInfoForBestUsed']['nonPaddedPriceInListingCurrencyValueOnly'])
+    if response['pricingInfoForBestNew'] is None:
+        new_shipping_cost = np.nan
+        dict_prices['new_shipping_cost'] = np.nan
+        dict_prices['new_price'] = np.nan
+    else:
+        new_shipping_cost = response['pricingInfoForBestNew']['domesticShippingPriceInPurchaseCurrencyWithCurrencySymbol'] if response['pricingInfoForBestNew']['vendorCountryNameInSurferLanguage'] =='United Kingdom' else response['pricingInfoForBestNew']['shippingToDestinationPriceInPurchaseCurrencyWithCurrencySymbol']
+        dict_prices['new_shipping_cost'] = float((re.compile(r'[^\d.,]+').sub('', new_shipping_cost)).replace(',','.'))
+        dict_prices['new_price'] = float(response['pricingInfoForBestNew']['nonPaddedPriceInListingCurrencyValueOnly'])
+    dict_prices['new_total'] = dict_prices['new_shipping_cost'] + dict_prices['new_price']
+    dict_prices['used_total'] = dict_prices['used_shipping_cost'] + dict_prices['used_price']
+    
+    url_base = 'https://www.abebooks.co.uk/servlet/SearchResults?'
+    df_book_options = pd.DataFrame(response['refinementList'])
+    
+    df_book_options['url'] = df_book_options['url'].str.replace('an=',url_base+'an=')
+    dict_response = {}
+    dict_response['pricing_info'] = dict_prices
+    dict_response['book_options'] = (df_book_options.to_dict('records'))
+    return dict_response
+
+
+def parse_generate_getHighlightInventoryForBookSearch(response : Dict) -> pd.DataFrame:
+    columns = list(response[0]['highlightedItemsMap'].keys())
+    df_response = pd.DataFrame()
+    for column in columns:
+        for i in range(len(response[0]['highlightedItemsMap'][column])):
+            df_response = pd.concat([df_response,pd.DataFrame([response[0]['highlightedItemsMap'][column][i]])])
+    df_response = df_response.drop(['bsaCodes','priceInDomainCurrency'],axis=1)
+    return df_response
--- utils.py
+++ utils.py
@@ -21,35 +21,60 @@ def check_response(json_response:json,request_type:str='price') -> Boolean:
             return True
         return False
 
-def parse_response_getPricingDataByISBN(list_responses: List) -> pd.DataFrame:
-    df_responses = pd.DataFrame()
-    
-    for response in list_responses:
-        dict_response = {}
-        dict_response['title'] = ''
-        dict_response['isbn'] = response['isbn']
-        dict_response['author'] = ''
-        dict_response['publisher'] = ''
-        dict_response['marketplace'] = 'abebooks'
-        if response['pricingInfoForBestUsed'] is None:
-            used_shipping_cost = np.nan
-            dict_response['used_shipping_cost'] = np.nan
-            dict_response['used_price'] = np.nan
-        else:
-            used_shipping_cost = response['pricingInfoForBestUsed']['domesticShippingPriceInPurchaseCurrencyWithCurrencySymbol'] if response['pricingInfoForBestUsed']['vendorCountryNameInSurferLanguage'] =='United Kingdom' else response['pricingInfoForBestUsed']['shippingToDestinationPriceInPurchaseCurrencyWithCurrencySymbol']
-            dict_response['used_shipping_cost'] = float((re.compile(r'[^\d.,]+').sub('', used_shipping_cost)).replace(',','.'))
-            dict_response['used_price'] = float(response['pricingInfoForBestUsed']['nonPaddedPriceInListingCurrencyValueOnly'])
-        if response['pricingInfoForBestNew'] is None:
-            new_shipping_cost = np.nan
-            dict_response['new_shipping_cost'] = np.nan
-            dict_response['new_price'] = np.nan
-        else:
-            new_shipping_cost = response['pricingInfoForBestNew']['domesticShippingPriceInPurchaseCurrencyWithCurrencySymbol'] if response['pricingInfoForBestNew']['vendorCountryNameInSurferLanguage'] =='United Kingdom' else response['pricingInfoForBestNew']['shippingToDestinationPriceInPurchaseCurrencyWithCurrencySymbol']
-            dict_response['new_shipping_cost'] = float((re.compile(r'[^\d.,]+').sub('', new_shipping_cost)).replace(',','.'))
-            dict_response['new_price'] = float(response['pricingInfoForBestNew']['nonPaddedPriceInListingCurrencyValueOnly'])
-        dict_response['new_total'] = dict_response['new_shipping_cost'] + dict_response['new_price']
-        dict_response['used_total'] = dict_response['used_shipping_cost'] + dict_response['used_price']
-        dict_response['urlPurchase'] = 'https://www.abebooks.co.uk/servlet/SearchResults?bi=0&bx=off&cm_sp=SearchF-_-Advtab1-_-Results&fe=off&ds=30&{}'.format(response['refinementList'][0]['url']) if response['success']==True else ''
-        df_responses = pd.concat([df_responses,pd.DataFrame(dict_response,index=[0])])
-    
-    return df_responses.reset_index(drop=True)
+def payload_generate_getPricingDataByISBN(list_isbns : List) ->List:
+    list_isbns = list_isbns if (isinstance(list_isbns,list)) else [list_isbns] # make sure user has is using list.
+    payloads = []
+    for isbn in list_isbns:
+        payload = {'action': 'getPricingDataByISBN',
+                'isbn': isbn,
+                'container': f'pricingService-{isbn}',
+                'User-Agent': "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/536.5 (KHTML, like Gecko) Chrome/19.0.1084.52 Safari/536.5",
+                'country':'GBP'
+                }
+        payloads.append(payload)
+    return payloads
+
+def payload_generate_getPricingDataForAuthorTitleByBinding(author:str,title:str,binding:str="soft") -> List:
+    """
+    Parameters:
+    - author (str) - book author name
+    - title (str) - book title
+    - binding(str) - this is the book bindingType
+    """
+    if binding.upper()=="HARD":
+        container = "priced-from-hard"
+    elif binding.upper()=="SOFT":
+        container = "priced-from-soft"
+    else:
+        raise ValueError("Invalid parameter. Binding must be either 'hard' or 'soft'")
+
+    payloads = [{'action': 'getPricingDataForAuthorTitleBindingRefinements',
+            'an': author,
+            'tn': title,
+            'container': container}]
+    return payloads
+
+
+def payload_generate_getPricingDataForAuthorTitleBDP(author:str,title:str,binding:str="soft") -> List:
+    """
+    Parameters:
+    - author (str) - book author name
+    - title (str) - book title
+    """
+    payloads = [{'action': 'getPricingDataForAuthorTitleBDP',
+            'an': author,
+            'tn': title,
+            'container': 'pricingService-'}]
+    return payloads
+
+def payload_generate_getBookRecommendationByISBN(list_isbns:List) -> List:
+    list_isbns = list_isbns if (isinstance(list_isbns,list)) else [list_isbns] # make sure user has is using list.
+    payloads = []
+    for isbn in list_isbns:
+        payload = {'pageId':'plp','itemIsbn13':isbn}
+        payloads.append(payload)
+    return payloads
+
+def payload_generate_getHighlightInventoryForBookSearch(key_word : str):
+    payload = [{'kn': key_word,'sortby': 30}]
+    return payload
